91483928cd8ca0b0bfff32962aee12539f6a0ab7
unknown <alanpaullo@yahoo.com.br>
2025-06-27 22:37:08 -0300

    alteracao driver

--- src/test/java/core/DriverFactory.java
+++ src/test/java/core/DriverFactory.java
@@ -32,7 +32,7 @@ public class DriverFactory {
                     WebDriverManager.chromedriver().setup();
 
                     ChromeOptions options = new ChromeOptions();
-                    //options.addArguments("--headless");
+                    options.addArguments("--headless");
                     options.addArguments("--disable-gpu");
                     options.addArguments("--window-size=1920,1080");
                     options.addArguments("--no-sandbox");
